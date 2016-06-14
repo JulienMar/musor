2eae06f3a739d52c227cfa92c4eb27be95dd7ea9
Julien Marbaix <julien.marbaix@hotmail.com>
2016-06-14 21:04:40 +0200

    Fix API key, old one removed, put it in config file

--- musor.py
+++ musor.py
@@ -1,19 +1,31 @@
 import pylast
 from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, TRCK, TCON, TDRC
+import configparser
 
-API_KEY = "628ab5741348060109a15df6939e138d"
-API_SECRET = "513578b1fe1f5cb49b10290b64badc64"
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+API_KEY = config['DEFAULT']['API_KEY']
+API_SECRET = config['DEFAULT']['API_SECRET']
 
 
 class Musor:
-    def set_album_names():
+    def set_album_names(dir):
         return 0
 
     def get_tracks(artist, album):
         network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
         album = network.get_album(artist, album)
-        for track in album.get_tracks():
-            print(track.get_tag)
+        return album.get_tracks()
+
+    def get_genre(artist, album):
+        network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
+        genres = network.get_album(artist, album).get_top_tags(5)
+        ALL_GENRES = TCON.GENRES
+        print(ALL_GENRES)
+        for genre in genres:
+            if ALL_GENRES.__contains__(genre[0].name.title()):
+                print(genre[0].name.title())
 
     def set_track_metadata(file, artist, title, number, album_name, genre, year):
         audio = ID3(file)
@@ -31,4 +43,4 @@ class Musor:
 
 
 #Musor.set_track_metadata("/home/julien/Documents/FunProjects/album1/Escape.mp3", "Netsk", "Escape", "1", "1", "drum and bass", "2010")
-Musor.get_tracks("Chet Faker", "Built on glass")
+Musor.get_genre("Chet faker", "built on glass")
