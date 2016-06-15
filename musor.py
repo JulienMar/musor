76d371337ef1a3f0999e80f85f89835331688fe6
Julien Marbaix <julien.marbaix@hotmail.com>
2016-06-15 22:45:41 +0200

    bringing it all together

--- musor.py
+++ musor.py
@@ -13,11 +13,13 @@ class Musor:
     def set_album_names(directory, album, artist):
         tracks = Musor.get_tracks(artist, album)
         for track in tracks:
-            print(tracks.index(track) + 1)
-            print(track)
-            matches = difflib.get_close_matches(track.title, os.listdir(directory))
-            print(matches)
-        
+            for realTrack in os.listdir(directory):
+                if track.title in realTrack:
+                    number = tracks.index(track)
+                    genre = Musor.get_genres(artist, album)
+                    album_name = track.title
+                    file_path = os.path.realpath(realTrack)
+
         os.rename(directory, os.path.dirname(os.path.abspath(directory))+ "/" + album)
 
     def get_tracks(artist, album):
@@ -25,23 +27,30 @@ class Musor:
         album = network.get_album(artist, album)
         return album.get_tracks()
 
-    def get_genre(artist, album):
+    def get_genres(artist, album):
         network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
         genres = network.get_album(artist, album).get_top_tags(5)
-        ALL_GENRES = TCON.GENRES
-        print(ALL_GENRES)
+        all_genres = TCON.GENRES
+        list_in_genres = []
         for genre in genres:
-            if ALL_GENRES.__contains__(genre[0].name.title()):
-                print(genre[0].name.title())
+            if all_genres.__contains__(genre[0].name.title()):
+                list_in_genres.append(genre)
+        return list_in_genres
 
-    def set_track_metadata(file, artist, title, number, album_name, genre, year):
+    #TODO: find why it returns none
+    def get_date(artist, album):
+        network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
+        album = network.get_album(artist, album).get_release_date()
+        print(album)
+
+    def set_track_metadata(file, artist, title, number, album_name, genres, year):
         audio = ID3(file)
         audio["TIT2"] = TIT2(encoding=3, text=title)
         audio["TPE2"] = TPE2(encoding=3, text=artist)
         #TODO: change to all artist
         audio["TALB"] = TALB(encoding=3, text=album_name)
         audio["TPE1"] = TPE1(encoding=3, text=artist)
-        audio["TCON"] = TCON(encoding=3, text=genre)
+        audio["TCON"] = TCON(encoding=3, text=genres)
         audio["TDRC"] = TDRC(encoding=3, text=year)
         audio["TRCK"] = TRCK(encoding=3, text=number)
         audio.save()
